aa1d1a7850817dd9d51009811426feda5e3d9ace
wenxiong <wenxiong@dreame.tech>
2026-01-13 15:11:35 +0800

    "导出StaggeredGrid组件并更新版本号至1.0.2"

--- lib/staggered_grid.dart
+++ lib/staggered_grid.dart
@@ -4,4 +4,5 @@ export 'package:staggered_grid/src/rendering/sliver_staggered_grid.dart';
 export 'package:staggered_grid/src/rendering/sliver_variable_size_box_adaptor.dart';
 export 'package:staggered_grid/src/rendering/tile_container_render_object_mixin.dart';
 export 'package:staggered_grid/src/widgets/sliver.dart';
+export 'package:staggered_grid/src/widgets/staggered_grid.dart';
 export 'package:staggered_grid/src/widgets/staggered_tile.dart';
